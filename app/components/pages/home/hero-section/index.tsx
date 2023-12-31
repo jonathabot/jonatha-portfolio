375b3348caef20119473795a1115d805e23a464d
Jonatha <lebotelhon@gmail.com>
2023-12-31 05:27:33 -0300

    integration of Hygraph with the project | consuming CSM API on Hero Section

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -1,50 +1,25 @@
 "use client";
 
 import { Button } from "@/app/components/button";
+import { CMSIcon } from "@/app/components/cms-icon";
+import { RichText } from "@/app/components/rich-text";
 import { TechBadge } from "@/app/components/tech-badge";
+import { HomePageInfo } from "@/app/types/page-info";
 import Image from "next/image";
 import { HiArrowNarrowRight } from "react-icons/hi";
-import {
-  TbBrandGithub,
-  TbBrandLinkedin,
-  TbBrandWhatsapp,
-  TbBrandYoutube,
-} from "react-icons/tb";
 
-const MOCK_CONTACTS = [
-  {
-    url: "https://github.com/jonathabot",
-    icon: <TbBrandGithub />,
-  },
-  {
-    url: "https://www.linkedin.com/in/jonathabotelho/",
-    icon: <TbBrandLinkedin />,
-  },
-  // {
-  //   url: "https://github.com.br",
-  //   icon: <TbBrandYoutube />,
-  // },
-  // {
-  //   url: "https://github.com.br",
-  //   icon: <TbBrandWhatsapp />,
-  // },
-];
+type HomeSectionProps = {
+  homeInfo: HomePageInfo;
+};
 
-const MOCK_KNOWN_TECHS = [
-  "Next.js",
-  "React.js",
-  "Tailwind CSS",
-  "TypeScript",
-  "JavaScript",
-];
-
-export const HeroSection = () => {
+export const HeroSection = ({ homeInfo }: HomeSectionProps) => {
   const handleContact = () => {
     const contactSection = document.querySelector("#contact");
     if (contactSection) {
       contactSection.scrollIntoView({ behavior: "smooth" });
     }
   };
+
   return (
     <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-40 lg:pb-[110px]">
       <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
@@ -52,18 +27,13 @@ export const HeroSection = () => {
           <p className="font-mono text-emerald-400">Olá, meu nome é</p>
           <h2 className="text-4xl font-medium mt-2">Jonatha Botelho</h2>
 
-          <p className="text-gray-400 my-6 text-sm sm:text-base">
-            Sou um Desenvolvedor Front-end Júnior com habilidade técnica e
-            profissional em tecnologias como Next.js, React.js, TypeScript,
-            JavaScript. Utilizo bibliotecas React, realizo consumo de API’s
-            públicas e privadas e possuo conhecimento em estrutura e
-            funcionamento de Banco de Dados MySQL. Estou sempre aberto a novas
-            oportunidades e desafios.
-          </p>
+          <div className="text-gray-400 my-6 text-sm sm:text-base">
+            <RichText content={homeInfo.introduction.raw} />
+          </div>
 
           <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px]">
-            {MOCK_KNOWN_TECHS.map((tech: any, index: number) => (
-              <TechBadge name={tech} key={index} />
+            {homeInfo.technologies.map((tech, index) => (
+              <TechBadge name={tech.name} key={index} />
             ))}
           </div>
 
@@ -74,14 +44,14 @@ export const HeroSection = () => {
             </Button>
 
             <div className="text-2xl text-gray-600 flex items-center h-20 gap-3">
-              {MOCK_CONTACTS.map((contact, index) => (
+              {homeInfo.socials.map((contact, index) => (
                 <a
                   href={contact.url}
                   key={`contact-${index}`}
                   target="_blank"
                   className="hover:text-gray-100 transition-colors"
                 >
-                  {contact.icon}
+                  <CMSIcon icon={contact.iconSvg} />
                 </a>
               ))}
             </div>
@@ -91,7 +61,7 @@ export const HeroSection = () => {
         <Image
           width={420}
           height={404}
-          src="/images/profile-pic.png"
+          src={homeInfo.profilePicture.url}
           alt="Foto de perfil do Jonatha Botelho"
           className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover"
         />
