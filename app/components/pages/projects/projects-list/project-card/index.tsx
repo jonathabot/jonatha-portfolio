30b38dea3b0d3fbbed25fb8e43265b9b6a2c5125
Jonatha <lebotelhon@gmail.com>
2024-01-01 06:32:00 -0300

    finished projects page

--- app/components/pages/projects/projects-list/project-card/index.tsx
+++ app/components/pages/projects/projects-list/project-card/index.tsx
@@ -1,14 +1,21 @@
+import { Project } from "@/app/types/projects";
 import Image from "next/image";
 
-export const ProjectCard = () => {
+type ProjectCardProps = {
+  project: Project;
+};
+
+export const ProjectCard = ({ project }: ProjectCardProps) => {
+  const technologies = project.technologies.map((x) => x.name).join(", ");
+
   return (
     <div className="rounded-lg h-[436px] flex flex-col bg-gray-800 overflow-hidden border-2 border-gray-800 hover:border-emerald-500 opacity-70 hover:opacity-100 transition-all group">
       <div className="w-full h-48 overflow-hidden">
         <Image
           width={380}
           height={200}
-          src="https://media.graphassets.com/FRhUdgUQTHmLmwf9u0BA"
-          alt="Thumbnailt do projeto BookWise"
+          src={project.thumbnail.url}
+          alt={`Thumbnail do project ${project.title}`}
           unoptimized
           className="w-full h-full object-cover group-hover:scale-110 duration-500 transition-all"
         />
@@ -16,15 +23,13 @@ export const ProjectCard = () => {
 
       <div className="flex-1 flex flex-col p-8">
         <strong className="font-medium text-gray-50/90 group-hover:text-emerald-500 transition-all">
-          BookWise
+          {project.title}
         </strong>
         <p className="mt-2 text-gray-400 line-clamp-4">
-          BookWise é uma plataforma de avaliação de livros que foi desenvolvida
-          durante o bootcamp Ignite da Rocketseat e foi muito legal aaaaa aaaaa
-          aaaa aaaaaaa dkaoskdopaskdopasko aosdkoaps kodap.
+          {project.shortDescription}
         </p>
         <span className="text-gray-300 text-sm font-medium block mt-auto truncate">
-          Next.js, Next Auth, Stitches, Radix, TypeScript, Prisma, React Query
+          {technologies}
         </span>
       </div>
     </div>
