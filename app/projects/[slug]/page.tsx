3a8895be0d3105ff9013fccc1a08423f78aa4958
Jonatha <lebotelhon@gmail.com>
2024-01-05 04:40:42 -0300

    changed fetchHygraphQuery revalidation

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -41,7 +41,7 @@ const getProjectDetails = async (slug: string): Promise<ProjectPageData> => {
   `;
   return fetchHygraphQuery(
     query,
-    1000 * 60 * 60 * 24 // 1 day
+    1 // 1 day
   );
 };
 
